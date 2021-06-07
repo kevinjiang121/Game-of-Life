ae12ea38749cae82da379e83aed9831dcf7512ee
Kevinjiang121 <kevinjiang121@gmail.com>
2021-06-07 18:48:34 -0400

    Added an option to repeat the game.

--- GameOfLife.py
+++ GameOfLife.py
@@ -6,9 +6,11 @@
 #If an alive square have 4 or more neighbors, it dies
 #If a dead square have exactly 3 neighbors, it lives
 import numpy as np
+import time
 
 g = np.zeros((10,10), dtype=int)
 g_temp = np.zeros((10,10), dtype=int)
+kill = 'y'
 
 def check(x,y): #general case
     count = 0
@@ -139,59 +141,72 @@ for x in range(10): #copies over g to g_temp
     for y in range(10):
         g_temp[x,y]=g[x,y]
 
-print(g)
-print("")
 
-q = "y" #stops loops for coordinates if answer is not y
-while(q=="y"): #ask for coordinates, must be in the form +int +int. Other froms will be rejected
-    ans = input("Enter coordinates for where life should be (ex: 2 2): ")
-    ans_array = ans.split()
-    ans_length =len(ans_array)
-    if(ans_length == 2):
-        try:
-            x = int(ans_array[0])
+while kill=="y":#repeats game if user does not quit
+    print(g)
+    print("")
+    q = "y"  # stops loops for coordinates if answer is not y
+    while q=="y": #ask for coordinates, must be in the form +int +int. Other froms will be rejected
+        ans = input("Enter coordinates for where life should be (ex: 2 2): ")
+        ans_array = ans.split()
+        ans_length =len(ans_array)
+        if(ans_length == 2):
             try:
-                y = int(ans_array[1])
-                if(x>0 and y>0 and x<11 and y<11):
-                    g[x,y] = 1
-                    print(g)
-                    print("")
-                    q = input("Do you want enter another coordinate (y/n): ")
+                x = int(ans_array[0])
+                try:
+                    y = int(ans_array[1])
+                    if(x>0 and y>0 and x<11 and y<11):
+                        g[x,y] = 1
+                        print(g)
+                        print("")
+                        q = input("Do you want enter another coordinate (y/n): ")
+                except ValueError:
+                    print("Integers must be between 1-10!")
             except ValueError:
                 print("Integers must be between 1-10!")
-        except ValueError:
-            print("Integers must be between 1-10!")
-    else:
-        print("Only 2 coordinates are allowed. Please try again.")
-
-repeat = input("Number of Iterations for the Game of Life: ") #number of lifecycles
-repeat_int = int(repeat)
-for i in range(repeat_int):
-    for x in range(10):
-        for y in range(10):
-            if g[x,y]==0: #case for when square is dead
-                game = neighbors(x,y)
-                if game==3:
-                    g_temp[x,y]=1;
-            if g[x,y]==1:
-                game = neighbors(x,y)
-                if game<2: #case for when square is alive
-                    g_temp[x,y]=0
-                elif game==2 or game==3:
-                    g_temp[x,y]=1
-                elif game>3:
-                    g_temp[x,y]=0
-                else: #should NEVER occur. Error statement in case something goes wrong
-                    print("ERROR Something went wrong in life! R.I.P.")
-    for x in range(10):
-        for y in range(10):
-            g[x, y] = g_temp[x, y] #copies g_temp over to g
+        else:
+            print("Only 2 coordinates are allowed. Please try again.")
     print("")
-    print(g)
-#for x in range(10):
-    #for y in range(10):
-#neighbors(1,2);
-
-
-
-#print(g);
+    repeat = input("Number of Iterations for the Game of Life: ") #number of lifecycles
+    repeat_int = int(repeat)
+    for i in range(repeat_int):
+        for x in range(10):
+            for y in range(10):
+                if g[x,y]==0: #case for when square is dead
+                    game = neighbors(x,y)
+                    if game==3:
+                        g_temp[x,y]=1;
+                if g[x,y]==1:
+                    game = neighbors(x,y)
+                    if game<2: #case for when square is alive
+                        g_temp[x,y]=0
+                    elif game==2 or game==3:
+                        g_temp[x,y]=1
+                    elif game>3:
+                        g_temp[x,y]=0
+                    else: #should NEVER occur. Error statement in case something goes wrong
+                        print("ERROR Something went wrong in life! R.I.P.")
+        for x in range(10):
+            for y in range(10):
+                g[x, y] = g_temp[x, y] #copies g_temp over to g
+        print("")
+        print(g)
+    kill = "w"
+    while kill != "n" and kill != "y": #determines if user will quit the game or not
+        kill = input("Continue? (y/n): ")
+        if kill == "y":
+            print("")
+            print("Starting new game...")
+            for x in range(10):
+                for y in range(10):
+                    g[x, y] = 0
+                    g_temp[x, y] = 0
+        elif kill == "n":
+            print("")
+            print("Quitting program...")
+        else:
+            print("Please enter valid input (y-yes, n-no): ")
+            print("")
+    #for x in range(10):
+        #for y in range(10):
+    #neighbors(1,2);
