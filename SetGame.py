0a0af49ae91d325b179c2a3d7bd1cd238a7819fc
Kevinjiang121 <kevinjiang121@gmail.com>
2021-06-18 13:29:29 -0400

    Fixed an issue with adding life to any of the edges of the grid. Also changed the input field from 0-9 to 1-10.

--- SetGame.py
+++ SetGame.py
@@ -2,11 +2,12 @@ import numpy as np
 
 g = np.zeros((10, 10), dtype=int)
 
+
 class SetGame:
     def __init__(self):
         pass
 
-    def createBoard(self):
+    def create_board(self):
         print(g)
         print("")
         q = "y"  # stops loops for coordinates if answer is not y
@@ -20,7 +21,7 @@ class SetGame:
                     try:
                         y = int(ans_array[1])
                         if 0 < x < 11 and 0 < y < 11:
-                            g[x-1, y-1] = 1
+                            g[x - 1, y - 1] = 1
                             print(g)
                             print("")
                             q = input("Do you want enter another coordinate (y/n): ")
