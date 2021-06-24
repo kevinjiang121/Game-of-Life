5a7e39452daf4339b41db889b30eece583f885de
Kevinjiang121 <kevinjiang121@gmail.com>
2021-06-24 00:56:39 -0400

    Rework GUI and made it look more like an actual game board. Got rid of any terminal interaction as as all interaction with the game state is now through the GUI.

--- GameOfLife.py
+++ GameOfLife.py
@@ -5,15 +5,16 @@
 # If an alive square have 2-3 neighbors, it lives
 # If an alive square have 4 or more neighbors, it dies
 # If a dead square have exactly 3 neighbors, it lives
+
 import numpy as np
 import tkinter as tk
-from SetGame import SetGame
 from PlayGame import PlayGame
 
 g = np.zeros((10, 10), dtype=int)
 g_temp = np.zeros((10, 10), dtype=int)
+
 root = tk.Tk()
-kill = 'y'
+entry = tk.Entry()
 
 
 def clear_board():
@@ -21,6 +22,7 @@ def clear_board():
         for y in range(10):
             g[x, y] = 0
             g_temp[x, y] = 0
+    display_game()
 
 
 def continue_game():
@@ -42,39 +44,56 @@ def continue_game():
 
 def next_loop():
     global g
-    g = play_game()
+    play = PlayGame(g, g_temp)
+    g = play.play_game()
+    display_game()
+
+
+def display_game():
     for r in range(10):
         for c in range(10):
-            tk.Label(root, text='%d' % (g[r, c]),
-                     borderwidth=1).grid(row=r, column=c)
+            if g[r, c] == 1:
+                tile = tk.Canvas(root, height=50, width=50, bg="black")
+            else:
+                tile = tk.Canvas(root, height=50, width=50, bg="white")
+            tile.grid(row=r, column=c)
 
 
-def play_game():
-    g_l = get_g()
-    play = PlayGame(g_l, g_temp)
-    game = play.play_game()
-    return game
-
-
-def get_g():
-    return g
+def insert_tile():
+    global g
+    global entry
+    give = entry.get()
+    give_life = give.split()
+    ans_length = len(give_life)
+    if ans_length == 2:
+        try:
+            x = int(give_life[0])
+            try:
+                y = int(give_life[1])
+                if 0 < x < 11 and 0 < y < 11:
+                    g[x - 1, y - 1] = 1
+                    print(g)
+            except ValueError:
+                print("Integers must be between 1-10!")
+        except ValueError:
+            print("Integers must be between 1-10!")
+    display_game()
 
 
 class GameOfLife:
-    global g
-    root.geometry('500x500')
-    btn = tk.Button(root, text='Next Life Cycle', bd='5', command=next_loop)
-    btn_close = tk.Button(root, text='Close', bd='5', command=root.destroy)
-
-    while kill == "y":  # repeats game if user does not quit
-        clear_board()
-        game = SetGame()
-        g = game.create_board()
-        for r in range(10):
-            for c in range(10):
-                tk.Label(root, text='%d' % (g[r, c]),
-                         borderwidth=1).grid(row=r, column=c)
-        btn.grid(row=11, column=11)
-        btn_close.grid(row=11, column=12)
-        root.mainloop()
-        kill = continue_game()
+    global entry
+    root.geometry('1980x1080')
+    enter_label = tk.Label(root, text="Please enter a coordinate from 1-10 (ex:2 2)")
+    btn = tk.Button(root, text='Next Life Cycle', bd='10', command=next_loop)
+    btn_close = tk.Button(root, text='Close', bd='10', command=root.destroy)
+    btn_clear = tk.Button(root, text='Clear Board', bd='10', command=clear_board)
+    entry = tk.Entry(root)
+    btn_insert = tk.Button(root, text='Insert', bd='10', command=insert_tile)
+    display_game()
+    btn.grid(row=11, column=12)
+    btn_close.grid(row=11, column=13)
+    btn_clear.grid(row=11, column=14)
+    enter_label.grid(row=12, column=11)
+    btn_insert.grid(row=13, column=12)
+    entry.grid(row=13, column=11)
+    root.mainloop()
