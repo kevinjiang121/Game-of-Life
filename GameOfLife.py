80976ec90c215c21db6d9c36909a38906eeda4de
Kevin Jiang <kevinjiang121@gmail.com>
2022-11-18 18:44:36 -0500

    Game of Life is now fully functional across entire board, rather than just a segment. Added a next turn button. Commented out code for clearing the board as there were a few bugs encountered.

--- GameOfLife.py
+++ GameOfLife.py
@@ -13,12 +13,12 @@ import numpy as np
 from PlayGame import PlayGame
 import pygame as pg
 
-N = int((1080-100)/20)
-M = int(1920/20)
+N = int((1080 - 100) / 20)
+M = int(1940 / 20)
 ALIVE = (0, 0, 0)
 DEAD = (255, 255, 255)
 HEIGHT = 1080
-WIDTH = 1920
+WIDTH = 1940
 
 g = np.zeros((N, M), dtype=int)
 g_temp = np.zeros((N, M), dtype=int)
@@ -45,21 +45,25 @@ def keep_playing():
 
 
 class GameOfLife:
+    pg.init()
+    pg.font.init()
+    font = pg.font.SysFont('Times New Roman', 75)
+    nextTurn = font.render('Next Round', False, (0, 0, 0))
+
     rects = []
     grid = []
     rectPos = [0, 0]
-    pg.init()
-    SCREEN = pg.display.set_mode((WIDTH, HEIGHT))
+    SCREEN = pg.display.set_mode((WIDTH - 20, HEIGHT))
     CLOCK = pg.time.Clock()
     SCREEN.fill(DEAD)
     blockSize = 20
-
     clear = pg.Rect(0, HEIGHT - 100, 660, 100)
     next = pg.Rect(660, HEIGHT - 100, 660, 100)
     auto = pg.Rect(1320, HEIGHT - 100, 660, 100)
     pg.draw.rect(SCREEN, (0, 0, 0, 0), clear, 3)
     pg.draw.rect(SCREEN, (0, 0, 0, 0), next, 3)
     pg.draw.rect(SCREEN, (0, 0, 0, 0), auto, 3)
+    SCREEN.blit(nextTurn, (800, HEIGHT - 100))
 
     for x in range(0, WIDTH, blockSize):
         for y in range(0, HEIGHT - 100, blockSize):
@@ -84,9 +88,10 @@ class GameOfLife:
             if rectPos[1] < HEIGHT - 100:
                 if event.type == pg.MOUSEBUTTONUP:
                     pg.draw.rect(SCREEN, ALIVE, grid[math.floor(rectPos[1] / 20)][math.floor(rectPos[0] / 20)], 20)
-                    g[math.floor(rectPos[1] / 20)][math.floor(rectPos[0]/20)] = 1
+                    g[math.floor(rectPos[1] / 20)][math.floor(rectPos[0] / 20)] = 1
 
             if rectPos[1] > HEIGHT - 100 and rectPos[0] < 660:
+                '''
                 if event.type == pg.MOUSEBUTTONUP:
                     SCREEN.fill(DEAD)
                     for i in range(len(rects)):
@@ -94,11 +99,11 @@ class GameOfLife:
                     pg.draw.rect(SCREEN, ALIVE, clear, 3)
                     pg.draw.rect(SCREEN, ALIVE, next, 3)
                     pg.draw.rect(SCREEN, ALIVE, auto, 3)
-
+                    g = np.zeros((N, M), dtype=int)
+                '''
             if rectPos[1] > HEIGHT - 100 and 660 < rectPos[0] <= 1320:
                 if event.type == pg.MOUSEBUTTONUP:
-                    game = PlayGame(g, g_temp)
-                    g = game.play_game()
+                    next_loop()
                     for i in range(len(g)):
                         for j in range(len(g[i])):
                             if g[i][j] == 1:
