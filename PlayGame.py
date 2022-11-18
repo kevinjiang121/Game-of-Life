8f88b3d81e1cce4829e6e7b3c1944946bfd3cd58
Kevin Jiang <kevinjiang121@gmail.com>
2022-11-18 18:42:50 -0500

    Changed size of board. Changed range in which the Game of Life is calculated for.

--- PlayGame.py
+++ PlayGame.py
@@ -2,7 +2,7 @@ import numpy as np
 from NeighborhoodScript import NeighborhoodScript
 
 N = int((1080-100)/20)
-M = int(1920/20)
+M = int(1940/20)
 g = np.zeros((N, N), dtype=int)
 g_temp = np.zeros((N, N), dtype=int)
 
@@ -16,14 +16,14 @@ class PlayGame:
     def play_game(self):
         self.change_board()
         for x in range(N):
-            for y in range(N):
+            for y in range(M-1):
                 self.g[x, y] = self.g_temp[x, y]  # copies g_temp over to g
         return self.g
 
     def change_board(self):
         neighbor = NeighborhoodScript(self.g)
         for x in range(N):
-            for y in range(N):
+            for y in range(M-1):
                 if self.g[x, y] == 0:  # case for when square is dead
                     game = neighbor.neighbors(x, y)
                     if game == 3:
